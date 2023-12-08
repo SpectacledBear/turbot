c8a850c34d384797d220f1121a77ebc66e76b4ea
Kyle Lewis <kyle@spectacledbear.ca>
2023-12-07 23:52:52 -0500

    Fixing bugs in timed_messages

--- timed_messages/timed_messages.py
+++ timed_messages/timed_messages.py
@@ -1,15 +1,12 @@
+import logging
 import math
 import os.path
 import time
 from collections import deque
 
-# Load messages
-# Determine message interval
-# Spread messages out
-# Return function that continues to trigger next message
-
-
 MESSAGES_FILENAME = "timed_messages/timed_messages.txt"
+
+logger = logging.getLogger(__name__)
 
 
 def interval(number_of_messages):
@@ -18,7 +15,11 @@ def interval(number_of_messages):
     if number_of_messages < 1:
         return seconds_in_hour
 
-    return math.floor(number_of_messages / seconds_in_hour)
+    interval_seconds = math.floor(seconds_in_hour / number_of_messages)
+
+    logger.debug(f"Setting interval to {interval_seconds} seconds.")
+
+    return interval_seconds
 
 
 def read_messages():
@@ -26,7 +27,13 @@ def read_messages():
         if os.path.isfile(MESSAGES_FILENAME) is False:
             raise FileExistsError("Messages file does not exist.")
 
-        messages = messages_file.readlines()
+        messages = []
+
+        logger.debug("Reading messages from text file.")
+        lines = messages_file.readlines()
+
+        for line in lines:
+            messages.append(line.strip())
 
         return messages
 
@@ -38,7 +45,7 @@ class TimedMessages:
         self.messages = deque(read_messages())
 
         # Divide the number of seconds in an hour by the number of messages and drop precision
-        self.interval_seconds = math.floor(len(self.messages) / (60 * 60))
+        self.interval_seconds = interval(len(self.messages))
 
     def next_message(self):
         if len(self.messages) < 1:
@@ -55,6 +62,6 @@ class TimedMessages:
         while True:
             message = self.next_message()
 
-            time.sleep(self.interval_seconds)
-
             self.bot.send_private_message(message)
+
+            time.sleep(self.interval_seconds)
