1bfbeb94cef9e91e9db2367fd64c1afc9283e338
Kyle Lewis <kyle@spectacledbear.ca>
2023-12-07 23:54:36 -0500

    Determining author from incoming messages

--- twitch/twitch_bot.py
+++ twitch/twitch_bot.py
@@ -5,6 +5,15 @@ import irc.bot
 logger = logging.getLogger(__name__)
 
 
+def get_tags_dict(e):
+    tags_dict = {}
+
+    for tag in e.tags:
+        tags_dict[tag["key"]] = tag["value"]
+
+    return tags_dict
+
+
 class TwitchBot(irc.bot.SingleServerIRCBot):
     def __init__(self, channel, username, token, server, port=6667):
         logger.debug("Instantiating bot.")
@@ -22,19 +31,18 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
 
     def welcome_chatter_if_new(self, e):
         # Collapse array of single-key dictionaries into one dictionary
-        tags_dict = {}
-        for tag in e.tags:
-            tags_dict[tag["key"]] = tag["value"]
+        tags_dict = get_tags_dict(e)
 
         chatter = tags_dict["display-name"]
 
         if chatter is not None:
-            if chatter not in self.chatters and chatter is not self.channel:
+            if chatter not in self.chatters and f"#{chatter.lower()}" != self.channel.lower():
+                logger.info(f"user: #{chatter.lower()}")
+                logger.info(f"channel: {self.channel.lower()}")
                 logger.debug("Adding chatter to list of chatters.")
                 self.chatters.append(chatter)
 
                 logger.info(f'Welcoming new chatter "{chatter}".')
-                logger.debug("Sending hello message in IRC channel.")
                 c = self.connection
                 c.privmsg(self.channel, f"Hello {chatter}!")
 
@@ -62,18 +70,22 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
     def on_privmsg(self, c, e):
         logger.debug(f"e: {str(e)}")
 
+        tags_dict = get_tags_dict(e)
+        author = tags_dict["display-name"]
         message = e.arguments[0]
-        logger.info(f"private message: {message}")
+
+        logger.info(f"private message from {author}: {message}")
         self.do_command(e, message)
 
     def on_pubmsg(self, c, e):
         logger.debug(f"e: {str(e)}")
 
+        tags_dict = get_tags_dict(e)
+        author = tags_dict["display-name"]
         message = e.arguments[0]
-        logger.info(f"public message: {message}")
 
+        logger.info(f"public message from {author}: {message}")
         self.welcome_chatter_if_new(e)
-
         self.do_command(e, message)
 
     # def on_dccmsg(self, c, e):
@@ -87,6 +99,8 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
         if cmd.startswith("!"):
             command_word = cmd.split()[0]
             self.send_private_message(f"I do not know what to do with the command {command_word}.")
+
+    # Helper methods
 
     def send_private_message(self, message):
         c = self.connection
